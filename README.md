dd49bb0821a44b4acb44d7ceb025bfa524390686
Antonio <mirandaadnarim45@gmail.com>
2025-11-05 22:30:55 -0300

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo a fazer de fora a fora um projeto do zero, tendo como base o tabnews.com.br.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Maria eu te amo, sua cara de bunda! ❤️(e é aqui que eu vou fazer meu projeto do curso)</h1> 
+  return (
+    <h1>
+      Maria eu te amo, sua cara de bunda! ❤️(e é aqui que eu vou fazer meu
+      projeto do curso)
+    </h1>
+  );
 }
 
 export default Home;
