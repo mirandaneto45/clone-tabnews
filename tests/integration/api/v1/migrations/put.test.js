c3ef5b12f83365a771181b1b8a1564da881637b7
Antonio <mirandaadnarim45@gmail.com>
2026-01-16 18:36:51 -0300

    refactor: use `next-connect` in `/migrations` and `/status`

---
+++ tests/integration/api/v1/migrations/put.test.js
@@ -0,0 +1,27 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+});
+
+describe("PUT /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("MethodNotAllowedError", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
+      expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        message: "Método não permitido para este endpoint.",
+        name: "MethodNotAllowedError",
+        action:
+          "Verifique se o método HTTP enviado é válido para este endpoint.",
+        status_code: 405,
+      });
+    });
+  });
+});
