92805daa3304c42816b24ab71473d47e26dbb59c
mirandaneto45 <mirandaadnarim45@gmail.com>
2026-03-25 16:38:30 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -31,6 +31,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "userteste",
         email: "emailteste@gmail.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
