2b160366932edc6f7ba56b30d89d1ae7b62b5e52
mirandaneto45 <mirandaadnarim45@gmail.com>
2026-03-27 12:27:55 -0300

    feat: send activation email after `user` registration

---
+++ infra/migrations/1774622669339_create-user-activation-tokens.js
@@ -0,0 +1,38 @@
+exports.up = (pgm) => {
+  pgm.createTable("user_activation_tokens", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+
+    used_at: {
+      type: "timestamptz",
+      notNull: false,
+    },
+
+    user_id: {
+      type: "uuid",
+      notNull: true,
+    },
+
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+
+    expires_at: {
+      type: "timestamptz",
+      notNull: true,
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+exports.down = false;
